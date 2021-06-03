8c49d5e428888cba16e10212db85ecd2ce049b5a
xwarfare <49755561+xwarfare@users.noreply.github.com>
2021-06-02 20:10:16 -0500

    Update telegram-bot.py

--- telegram-bot.py
+++ telegram-bot.py
@@ -143,8 +143,8 @@ def ask(question, chat_log=None):
         chat_log = start_chat_log
     prompt = f'{chat_log}Human: {question}\nAI:'
     response = completion.create(
-        prompt=prompt, engine="davinci", stop=['\nHuman'], temperature=0.9,
-        top_p=1, frequency_penalty=0, presence_penalty=0.6, best_of=1,
+        prompt=prompt, engine="davinci", stop=['\nHuman'], temperature=0.7,
+        top_p=0.7, frequency_penalty=0, presence_penalty=0.6, best_of=1,
         max_tokens=150)
     answer = response.choices[0].text.strip()
     return answer
